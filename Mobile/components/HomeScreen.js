ec00a57d7bf5815bad42d8c5d767f20824a40eb4
adamkoot <adam.kot511@gmail.com>
2022-11-19 22:44:03 +0100

    fix styles

--- Mobile/components/HomeScreen.js
+++ Mobile/components/HomeScreen.js
@@ -1,54 +1,74 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, ImageBackground, TouchableOpacity, Alert, ActivityIndicator } from "react-native";
+import {
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import Bg from "../assets/backgroundStart.jpg";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { validate } from "react-native-web/dist/cjs/exports/StyleSheet/validate";
 
 export default function HomeScreen({ navigation }) {
-
-  const [getToken, setGetToken] = useState(false)
+  const [getToken, setGetToken] = useState(false);
 
   useEffect(() => {
     const navigateUser = (res, token) => {
       if ("user" in res) {
-        navigation.navigate("MainMenu", { token: token, userData: res.user })
+        navigation.navigate("MainMenu", {
+          token: token,
+          userData: res.user,
+          logOut: false,
+        });
       } else {
-        AsyncStorage.removeItem("access_token")
-        setGetToken(true)
+        AsyncStorage.removeItem("access_token");
+        setGetToken(true);
       }
-    }
+    };
 
     const validateUser = (token) => {
-      const url = "https://markow.pl/API/public/api/user/me"
+      const url = "https://markow.pl/API/public/api/user/me";
       fetch(url, {
         headers: {
           Authorization: "Bearer " + token,
-          'Accept': 'application/json',
-        }
+          Accept: "application/json",
+        },
       })
-        .then(res => res.json())
-        .then(res => navigateUser(res, token))
-    }
+        .then((res) => res.json())
+        .then((res) => navigateUser(res, token));
+    };
 
     const getToken = async () => {
       try {
-        const token = await AsyncStorage.getItem("access_token")
+        const token = await AsyncStorage.getItem("access_token");
 
         if (token != undefined) {
-          validateUser(token)
+          validateUser(token);
         } else {
-          setGetToken(true)
+          setGetToken(true);
         }
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
-    getToken()
-  }, [])
+    };
+    getToken();
+  }, []);
 
   return (
     <ImageBackground source={Bg} style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 10 }}>
-        <Text style={{ color: "white", padding: 15, fontSize: 48, fontWeight: "bold" }}>PickApp</Text>
+      <View style={{ flex: 3, padding: 25 }}>
+        <Text
+          style={{
+            color: "white",
+            padding: 0,
+            fontSize: 48,
+            fontWeight: "bold",
+          }}
+        >
+          PickApp
+        </Text>
       </View>
       {getToken ? (
         <>
@@ -57,15 +77,18 @@ export default function HomeScreen({ navigation }) {
               flex: 8,
               flexDirection: "column",
               justifyContent: "flex-end",
-              padding: 10,
-            }}>
-            <Text style={{ color: "white", fontSize: 36 }}>Reduce food waste</Text>
+              paddingHorizontal: 15,
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 36 }}>
+              Reduce food waste
+            </Text>
             <Text style={{ color: "white", opacity: 0.7, fontSize: 16 }}>
-              Polish households waste 2 million tons of food per year, meaning an
-              estimated 54 kg per Polish citizen per year.
+              Polish households waste 2 million tons of food per year, meaning
+              an estimated 54 kg per Polish citizen per year.
             </Text>
           </View>
-          <View style={{ flex: 3 }}>
+          <View style={{ flex: 4, paddingHorizontal: 15, paddingTop: 10 }}>
             <TouchableOpacity
               style={{
                 alignItems: "center",
@@ -73,13 +96,16 @@ export default function HomeScreen({ navigation }) {
                 backgroundColor: "#FFFFFF",
                 borderRadius: 9,
                 height: 60,
-                padding: 10,
+
                 margin: 5,
               }}
               onPress={() => {
                 navigation.navigate("Login");
-              }}>
-              <Text style={{ fontSize: 20, color: "#59981A", fontWeight: "bold" }}>
+              }}
+            >
+              <Text
+                style={{ fontSize: 20, color: "#59981A", fontWeight: "bold" }}
+              >
                 Log in
               </Text>
             </TouchableOpacity>
@@ -92,25 +118,32 @@ export default function HomeScreen({ navigation }) {
                 borderColor: "white",
                 borderRadius: 9,
                 height: 60,
-                padding: 10,
+
                 margin: 5,
               }}
               onPress={() => {
                 navigation.navigate("Register");
-              }}>
-              <Text style={{ fontSize: 20, color: "#FFFFFF", fontWeight: "bold" }}>
+              }}
+            >
+              <Text
+                style={{ fontSize: 20, color: "#FFFFFF", fontWeight: "bold" }}
+              >
                 Sign up
               </Text>
             </TouchableOpacity>
-          </View></>) : (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-start" }}>
+          </View>
+        </>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+        >
           <ActivityIndicator color="#3d550c" size="large" />
         </View>
-      )
-
-      }
-
-
+      )}
     </ImageBackground>
   );
 }
